fbfd1a76498c9c9b523cbdf0f543ca4bc3b8f147
Alyson Fox <fox33@llnl.gov>
2018-12-07 09:42:07 -0800

    creating a WIP pull request to for socket communication. Changed socked factory to be generalized.

--- include/devices/Skynet_SocketCommunicatorFactory.hpp
+++ include/devices/Skynet_SocketCommunicatorFactory.hpp
@@ -11,15 +11,24 @@ namespace skynet
   class SocketCommunicatorFactory : public CommunicatorFactory
   {
   public:
+     /** \brief Create a new SocketCommunicatorFactory.
+     *
+     * The ip_address of the device we wish to communicated is needed.
+     */
+    SocketCommunicatorFactory(std::string ip_add)
+      : ip_address_(ip_add)
+    { }
+
+
     std::unique_ptr<DeviceCommunicator> 
     create_new_communicator(std::vector<std::string> comm_config_info)
     {
-      std::string ip_address = "192.0.0.1";
+      // std::string ip_address = "192.0.0.1";
       return SocketCommunicator(ip_address);
     }
 
   private:
-
+  std::string ip_address_
   }; // SocketCommunicatorFactory
 } // namespace skynet
 
