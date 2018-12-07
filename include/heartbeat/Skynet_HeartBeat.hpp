a3e7b5e67eb0bf7eeea91a89f9623bc74c190fd5
Alyson Fox <fox33@llnl.gov>
2018-12-07 08:46:58 -0800

    testing updating Amy's branch on her fork

---
+++ include/heartbeat/Skynet_HeartBeat.hpp
@@ -0,0 +1,26 @@
+#ifndef SKYNET_HEARTBEAT_HPP__
+#define SKYNET_HEARTBEAT_HPP__
+
+#include <utility> //Is this used?
+#include <vector>
+
+namespace skynet
+{
+  /** \class HeartBeat
+   *  \brief Abstract class for operting the heartbeat
+   *
+   */
+   class HeartBeat
+   {
+   public:
+
+    
+   private:
+
+   };// class 
+
+
+}// namespace skynet
+
+#endif /* SKYNET_HEARTBEAT_HPP__ */
+
