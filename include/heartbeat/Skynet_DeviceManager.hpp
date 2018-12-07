a63a040a73706927e2bb98630211c48c3d1e3db1
Musselman <musselman5@llnl.gov>
2018-12-06 16:03:04 -0800

    Cleaned up a few things

--- include/heartbeat/Skynet_DeviceManager.hpp
+++ include/heartbeat/Skynet_DeviceManager.hpp
@@ -1,47 +1,83 @@
 #ifndef SKYNET_DEVICEMANANGER_HPP__
 #define SKYNET_DEVICEMANANGER_HPP__
 
-#include <utility>
+#include <utility> //Is this used?
 #include <vector>
+#include "devices/Skynet_DeviceCommunicator.hpp"
+#include "devices/Skynet_DeviceReference.hpp"
 
 namespace skynet
 {
+  /** \class DeviceManager
+   *  \brief Abstract class for keeping track of devices
+   *
+   * This abstract class provides an interface for keeping
+   * track of neighboring devices and the communication type
+   * associated with these devices.
+   */
+   class DeviceManager
+   {
+   public:
 
-	class DeviceManager
-	{
-	public:
-	  virtual ~DeviceManager() = default;
+     /** \brief Construct a new DeviceManager
+      *
+      *	\param nearby_devices a vector of devices that this device
+      *		will keep track of.
+      * QUESTION: Before Colin had std::move(nearby_devices) here.
+      * 	I'm not sure why. Should we do that instead?
+      */
+     DeviceManager(std::vector<DeviceReference> nearby_devices)
+       : nearby_devices_(nearby_devices){}
+     
+     virtual ~DeviceManager() = default;
 
-	  unsigned long number_of_devices() const
-	  {
-	    return do_get_number_of_devices();
-	  }
+     unsigned long get_number_of_devices() const
+     {
+       return do_get_number_of_devices();
+     }
 
-	  DeviceCommunicator get_comunication_type() const
-	  {
-	    return do_get_comunication_type();
-	  }
+     std::vector<DeviceReference> get_device_list() const
+     {
+       return nearby_devices_;
+     }
+
+     
+     /**NOTE: Changed get_communicator_type to get_com_list since
+      * (I believe) the communicator type doesn't have to be the same 
+      * for all devices in your device list. Also DeviceReference already
+      * has a function to get the device communicator, so I'm questioning
+      * whether we need this here at all.
+      */ 
+     DeviceCommunicator get_comm_list() const
+     {
+       return do_get_comm_list();
+     }
+
+   private:
+
+     virtual unsigned long do_get_number_of_devices() const = 0;
+
+     std::vector<DeviceCommunicator&> do_get_comm_list() const
+     {
+       std::vector<DeviceReference> device_list = get_device_list();
+
+       std::vector<DeviceCommunicator> comm_list;
+       
+       for(std::vector<DeviceReference>::iterator it = device_list.begin(); it != device_list.end(); ++it){
+	 comm_list.push_back(it.get_comm());
+       }
+
+       return comm_list;
+     }
 
 
-	  std::vector<Device> get_device_list() const
-	  {
-	    return do_get_comunication_type();
-	  }
+   private:
+     std::vector<DeviceReference> nearby_devices_;
 
-	private:
-
-	 virtual unsigned long do_get_number_of_devices() const = 0;
-
-	 virtual DeviceCommunicator do_get_comunication_type() const = 0;
-
-	
-	private:
-		std::vector<Device> nearby_devices_;
-
-
-	};// class 
+   };// class 
 
 
 }// namespace skynet
 
 #endif /* SKYNET_DEVICEMANANGER_HPP__ */
+
