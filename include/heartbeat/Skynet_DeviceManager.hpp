13d1f2d560e038edfff50879f5d39bdea0b2d84f
Musselman <musselman5@llnl.gov>
2018-12-14 17:47:00 -0800

    Removed get_number_of_devices and git_comm_list functions and split get_device_list function into get_known_devices and get_live_devices

--- include/heartbeat/Skynet_DeviceManager.hpp
+++ include/heartbeat/Skynet_DeviceManager.hpp
@@ -1,8 +1,9 @@
-#ifndef SKYNET_DEVICEMANANGER_HPP__
+A#ifndef SKYNET_DEVICEMANANGER_HPP__
 #define SKYNET_DEVICEMANANGER_HPP__
 
-#include <utility> //Is this used?
 #include <vector>
+#include <algorithm> //For remove function used to remove devices from device used
+
 #include "devices/Skynet_DeviceCommunicator.hpp"
 #include "devices/Skynet_DeviceReference.hpp"
 
@@ -21,71 +22,69 @@ namespace skynet
 
      /** \brief Construct a new DeviceManager
       *
-      *	\param nearby_devices a vector of devices that this device
-      *		will keep track of.
-      * QUESTION: Before Colin had std::move(nearby_devices) here.
-      * 	I'm not sure why. Should we do that instead?
+      *	\param nearby_devices a vector of devices that DeviceManager
+      *	       will keep track of.
       */
-     template<typename T>
-     DeviceManager(const T& config) const
+     DeviceManager(const std::vector<DeviceReference> nearby_devices)
+       : nearby_devices(nearby_devices_) const
      {
-       set_device_list(config);
      }
-     
+
+     /** \brief DeviceManager destructor
+      */
      virtual ~DeviceManager() = default;
 
-     template<typename T>
-     virtual void set_device_list(const T& config) const = 0;
-     
-     //Get number of nearby devices
-     unsigned long get_number_of_devices() const
-     {
-       return do_get_number_of_devices();
-     }
-
-     //Get set of nearby devices
-     std::vector<DeviceReference> get_device_list() const
+     /** \brief Get devices that the DeviceManager is currently keeping
+      *	track of
+      *
+      * The complete set of known devices  may include dead devices if 
+      *	the DeviceManager continues to track devices that have been
+      * pronounced dead.
+      *
+      * \return a vector of DeviceReferences
+      */
+     std::vector<DeviceReference> get_known_devices() const
      {
        return nearby_devices_;
      }
 
-     //Add a device to the nearby_device list
-     //Question: should we have do_add_device and do_remove device
-     // functions in the private section?
-     virtual void add_device(DeviceReference new_device) const = 0;
-
-     //Remove a device from the nearby_device list
-     virtual void remove_device(DeviceReference old_device) const = 0;
-
-     //Get list of communicator types for nearby devices
-     /**NOTE: Changed get_communicator_type to get_com_list since
-      * (I believe) the communicator type doesn't have to be the same 
-      * for all devices in your device list. Also DeviceReference already
-      * has a function to get the device communicator, so I'm questioning
-      * whether we need this here at all.
-      */ 
-     DeviceCommunicator get_comm_list() const
+     /** \brief Get devices that DeviceManager currently believes to be live
+      *
+      * \return a vector of DeviceReferences
+      */
+     std::vector<DeviceReference> get_live_devices() const
      {
-       return do_get_comm_list();
+       return do_get_live_devices();
      }
+
+     /** Add a device to the nearby devices list
+      *
+      * \param new_device Device to add to the nearby devices list
+      */
+     void add_device(DeviceReference new_device) const
+     {
+       nearby_devices_.push_back(new_device);
+     }
+
+     /*Remove a device from the nearby devices list
+      *
+      * \param old_device device remove from nearby devices list
+      */
+     void remove_device(DeviceReference old_device) const
+     {
+       //Use erase on top of remove to shorten vector after removing device
+       nearby_devices.erase(std::remove(nearby_devices.begin(), nearby_devices.end(), old_device), nearby_devices_.end());
+     }
+
 
    private:
 
-     virtual unsigned long do_get_number_of_devices() const = 0;
-
-     std::vector<DeviceCommunicator&> do_get_comm_list() const
-     {
-       std::vector<DeviceReference> device_list = get_device_list();
-
-       std::vector<DeviceCommunicator> comm_list;
-       
-       for(std::vector<DeviceReference>::iterator it = device_list.begin(); it != device_list.end(); ++it){
-	 comm_list.push_back(it.get_comm());
-       }
-
-       return comm_list;
-     }
-
+     /** Get devices that the device manager believes to be live
+      * If the DeviceManager only keeps track of live devices this function
+      * will be equivalent to get_live_devices.
+      */
+     virtual std::vector<DeviceReference> do_get_live_devices() const = 0;
+     
 
    private:
      std::vector<DeviceReference> nearby_devices_;
