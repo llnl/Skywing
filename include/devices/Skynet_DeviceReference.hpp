4ad6d953e06fd04d502f764a1185ef2dd25eba3d
Chris Vogl <vogl2@llnl.gov>
2018-12-17 15:11:52 -0800

    clean up of errors in Skynet_DeviceReference

--- include/devices/Skynet_DeviceReference.hpp
+++ include/devices/Skynet_DeviceReference.hpp
@@ -23,37 +23,12 @@ namespace skynet
   public:
     /** \brief Construct a new \c DeviceReference.
      *
-     * \param comm A DeviceCommunicator representing this
+     * \param comm_factory A CommuncatorFactory representing this
      * DeviceReference's communications policy.
      */
-    // TODO resolve compiler error and uncomment
-    // error: use of deleted function ‘std::unique_ptr<_Tp, _Dp>&
-    // std::unique_ptr<_Tp, _Dp>::operator=(const std::unique_ptr<_Tp, _Dp>&)
-    // [with _Tp = skynet::CommunicatorFactory; _Dp =
-    // std::default_delete<skynet::CommunicatorFactory>]’
-    // { comm_factory_ = comm_factory}
-    //                   ^~~~~~~~~~~~
-    // In file included from /home/vogl2/local/gcc-6.5.0_tux/include/c++/6.5.0/memory:81:0,
-    //             from /home/vogl2/local/gcc-6.5.0_tux/include/c++/6.5.0/thread:40,
-    //             from /home/vogl2/workspace/skynet/include/heartbeat/Skynet_Heart.hpp:6,
-    //             from HeartHelper.hpp:7,
-    //             from skynet_startup.cpp:22:
-    // /home/vogl2/local/gcc-6.5.0_tux/include/c++/6.5.0/bits/unique_ptr.h:362:19: note: declared here
-    //   unique_ptr& operator=(const unique_ptr&) = delete;
-    //
     DeviceReference(std::unique_ptr<CommunicatorFactory> comm_factory)
-      //: is_believed_live_(true), comm_factory_(comm_factory)
+      : is_believed_live_(true), comm_factory_(std::move(comm_factory))
     { }
-
-    /** \brief Get the DeviceCommunicator policy object. */
-    // TODO: resolve compiler error and uncomment
-    // error: invalid initialization of reference of type
-    // ‘const skynet::DeviceCommunicator&’ from expression of type
-    // ‘skynet::CommunicatorFactory’
-    // { return *comm_factory_; }
-    //
-    //const DeviceCommunicator& get_comm() const
-    //{ return *comm_factory_; }
 
     /** \brief Get if we believe the referred device to be live. */
     bool get_is_belived_live() const
