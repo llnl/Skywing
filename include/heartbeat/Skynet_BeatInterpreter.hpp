6ae7594a0328272cdccd1ab4606f5fa907d97010
Musselman <musselman5@llnl.gov>
2018-12-18 12:27:44 -0800

    Changed determine_device_status function to should_device_remain

--- include/heartbeat/Skynet_BeatInterpreter.hpp
+++ include/heartbeat/Skynet_BeatInterpreter.hpp
@@ -1,6 +1,8 @@
 #ifndef SKYNET_BEATINTERPRETER_HPP__
 #define SKYNET_BEATINTERPRETER_HPP__
 
+#include "devices/DeviceReference.hpp"
+#include "Skynet_BeatSender.hpp"
 
 namespace skynet
 {
@@ -12,19 +14,21 @@ namespace skynet
    class BeatInterpreter
    {
    public:
-
+    
      /** \brief Determine the status of a device based on its
       * responses to the heartbeat
       *
       * \param device the device that we want to determine the status of
-      * \param response_history History (probably a vector) of responses
-      *	from the device that will be used to determine device status
+      * \param response_history Vector of responses from the device to 
+      *	 be used in determining device status
       *
       * \return 1 if device is alive or at least we are not ready to
-      *	pronounce it dead, 0 if device is determined to be dead
+      *	 pronounce it dead, 0 if we have decided device is dead
       */
-     template<typename T>
-     bool determine_device_status(const DeviceReference& device, const T& response_history);
+     //TO DO: Figure out where response_history should be stored
+     virtual bool should_device_remain(const DeviceReference& device,
+				       const std::vector<BeatSender::BeatResponse>& response_history)
+     	                               = 0;
 
    private:
 
