39b05cf29e974108eb01b7d42d54ff02eba1fd0b
Chris Vogl <vogl2@llnl.gov>
2018-12-17 16:54:04 -0800

    Refactor of ns3 HeartHelper to be header-only

--- src/ns3/HeartHelper.hpp
+++ src/ns3/HeartHelper.hpp
@@ -4,14 +4,31 @@
 #include "ns3/application-container.h"
 #include "ns3/node-container.h"
 #include "ns3/object-factory.h"
+#include "Ns3_Heart.hpp"
 
 namespace ns3
 {
   class HeartHelper
   {
   public:
-    HeartHelper();
-    ApplicationContainer Install(NodeContainer c) const;
+    HeartHelper()
+    {
+      factory_.SetTypeId(Heart::GetTypeId());
+    }
+
+    ApplicationContainer Install(NodeContainer c) const
+    {
+      ApplicationContainer apps;
+      for (NodeContainer::Iterator i = c.Begin(); i != c.End(); i++)
+      {
+        Ptr<Node> node = *i;
+        Ptr<Application> app = factory_.Create<Heart>();
+        node->AddApplication(app);
+        apps.Add(app);
+      }
+      return apps;
+    }
+
   private:
     ObjectFactory factory_;
   }; // class HeartHelper
