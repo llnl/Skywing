794947b6399ed57346667b4d280e94280c0909a6
Musselman <musselman5@llnl.gov>
2018-12-21 16:53:10 -0800

    Added get_id, update device status, and == operator functions

--- include/devices/Skynet_DeviceReference.hpp
+++ include/devices/Skynet_DeviceReference.hpp
@@ -11,6 +11,12 @@
 
 namespace skynet
 {
+
+  //Some types that will be used by DeviceReference
+  //NOTE: For the == operator to work below, id_t must be a type for
+  // which comparison is defined.
+  typedef unsigned int id_t; //Device ID type
+  
   /** \class DeviceReference
 
       A DeviceReference object represents some other participating
@@ -26,15 +32,39 @@ namespace skynet
      * \param comm_factory A CommuncatorFactory representing this
      * DeviceReference's communications policy.
      */
-    DeviceReference(std::unique_ptr<CommunicatorFactory> comm_factory)
-      : is_believed_live_(true), comm_factory_(std::move(comm_factory))
+    DeviceReference(id_t device_id,
+		    std::unique_ptr<CommunicatorFactory> comm_factory)
+      : is_believed_live_(true), device_id_(std::move(device_id)),
+	comm_factory_(std::move(comm_factory))
     { }
-
+    
     /** \brief Get if we believe the referred device to be live. */
-    bool get_is_belived_live() const
+    bool get_is_believed_live() const
     { return is_believed_live_; }
 
+    /** \brief Update is_believed_live when new information is gained
+     *   about the device status
+     */
+    void set_is_believed_live(bool status)
+    {
+      is_believed_live = status;
+    }    
 
+    /** Get device ID */
+    const id_t get_id()
+    {
+      return device_id_;
+    }
+
+    bool operator ==(const DeviceReference& other_device) const
+    {
+      id_t other_id = other_device.get_id();
+      if (device_id_ == other_id){
+	return 1;
+      }
+      return 0;
+    }
+    
     /** \brief Request a new DeviceCommunicator for this DeviceReference.
      * \return A new DeviceCommunicator. */
     std::unique_ptr<DeviceCommunicator> create_new_communicator()
