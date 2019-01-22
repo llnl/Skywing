51cd61b59098bab1bd46e8ae883f7de293c5f9a0
Chris Vogl <vogl2@llnl.gov>
2019-01-22 14:09:42 -0800

    created DeviceIdRegistry test and modified DeviceIdRegistry to pass the tests

--- include/heartbeat/Skynet_DeviceIdRegistry.hpp
+++ include/heartbeat/Skynet_DeviceIdRegistry.hpp
@@ -13,27 +13,24 @@ namespace skynet
   public:
 
     DeviceIdRegistry()
-    {
-      set_bounds();
-      next_free_id_ = first_id_;
-    }
+    { set_bounds(); }
 
     const id_t next_id()
     {
+      const id_t next_free_id_ = next_free_id();
       registered_ids_.push_back(next_free_id_);
-      const id_t registered_id = next_free_id_;
-      set_next_free_id();
-      return registered_id;
+      return next_free_id_;
     }
 
     void free_id(id_t id_to_free)
     {
-      for (id_t id : registered_ids_)
+      for (typename std::vector<id_t>::iterator iter = registered_ids_.begin();
+        iter != registered_ids_.end(); iter++)
       {
-        if (id == id_to_free)
+        if (*iter == id_to_free)
         {
-          freed_ids_.push_back(id);
-          registered_ids_.erase(id);
+          freed_ids_.push_back(*iter);
+          registered_ids_.erase(iter);
         }
       }
     }
@@ -44,19 +41,25 @@ namespace skynet
 
     void set_bounds();
 
-    void set_next_free_id()
+    const id_t next_free_id()
     {
-      // first check if any ids have been freed
-      if (!freed_ids_.empty())
+      id_t next_free_id_;
+      // first, check if any ids have been registered
+      if (registered_ids_.empty())
+        next_free_id_ = first_id_;
+      // second, check if any ids have been freed
+      else if (!freed_ids_.empty())
       {
         next_free_id_ = freed_ids_.back();
         freed_ids_.pop_back();
       }
+      // third, use next_value to advance id value until a free one is found
       else
       {
         bool is_free;
         bool found = false;
         int reset_count = 0;
+        next_free_id_ = registered_ids_.back();
         while (!found && reset_count < 2)
         {
           if (next_free_id_ == last_id_)
@@ -83,19 +86,19 @@ namespace skynet
           exit(-1);
         }
       }
+      return next_free_id_;
     }
 
     id_t first_id_;
     id_t last_id_;
-    id_t next_free_id_;
     std::vector<id_t> registered_ids_;
     std::vector<id_t> freed_ids_;
 
   }; // class DeviceIdRegistry
 
-  template<> void DeviceIdRegistry<uint8_t>::set_bounds()
+  template<> inline void DeviceIdRegistry<uint8_t>::set_bounds()
   { first_id_ = 0; last_id_ = UINT8_MAX; }
-  template<> uint8_t DeviceIdRegistry<uint8_t>::next_value(uint8_t id)
+  template<> inline uint8_t DeviceIdRegistry<uint8_t>::next_value(uint8_t id)
   { return id+1; }
 
 } // namespace skynet
