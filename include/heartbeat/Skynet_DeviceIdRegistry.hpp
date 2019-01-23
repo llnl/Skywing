6ab7c7cc6fce915058aa31d8ac69bf382f9020e8
Chris Vogl <vogl2@llnl.gov>
2019-01-22 16:07:45 -0800

    added comments to DeviceIdRegistry.hpp

--- include/heartbeat/Skynet_DeviceIdRegistry.hpp
+++ include/heartbeat/Skynet_DeviceIdRegistry.hpp
@@ -5,7 +5,12 @@
 
 namespace skynet
 {
-
+  /** \class DeviceIdRegistry
+   *  \brief Class for managing device IDs
+   *
+   * This templated class implements a registry of device IDs where the
+   * template type specifies the ID value type.
+   */
   template<typename id_t>
   class DeviceIdRegistry
   {
@@ -15,6 +20,9 @@ namespace skynet
     DeviceIdRegistry()
     { set_bounds(); }
 
+    /** \brief Obtain the next free ID in the registry
+     *  \return The value of the next free ID
+     */
     const id_t next_id()
     {
       const id_t next_free_id_ = next_free_id();
@@ -22,8 +30,12 @@ namespace skynet
       return next_free_id_;
     }
 
+    /** \brief Free a registered ID so that it can be reused
+     *  \param id_to_free The value of the ID to be freed
+     */
     void free_id(id_t id_to_free)
     {
+      bool found = false;
       for (typename std::vector<id_t>::iterator iter = registered_ids_.begin();
         iter != registered_ids_.end(); iter++)
       {
@@ -31,17 +43,32 @@ namespace skynet
         {
           freed_ids_.push_back(*iter);
           registered_ids_.erase(iter);
+          found = true;
           break;
         }
+      }
+      if (!found)
+      {
+        printf("Tried to free ID that was not registered in DeviceIdRegistry\n");
+        exit(-1);
       }
     }
 
   private:
 
+    /** \brief Given a device ID value, obtain the next value
+     *  \param id Current value of ID
+     *  \return The value of the next ID, regardless of whether it is registered
+     */
     id_t next_value(id_t id);
 
+    /** \brief Set bounds of ID type (first_id_) and (last_id_)
+     */
     void set_bounds();
 
+    /** \brief Find the next free ID
+     *  \return The value of the next free ID
+     */
     const id_t next_free_id()
     {
       id_t next_free_id_;
@@ -89,6 +116,7 @@ namespace skynet
 
   }; // class DeviceIdRegistry
 
+  // Implementation for uint8_t IDs
   template<> inline void DeviceIdRegistry<uint8_t>::set_bounds()
   { first_id_ = 0; last_id_ = UINT8_MAX; }
   template<> inline uint8_t DeviceIdRegistry<uint8_t>::next_value(uint8_t id)
