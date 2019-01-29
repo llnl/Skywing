741988317134b5c50b0200f233e5f913844ede6e
Musselman <musselman5@llnl.gov>
2019-01-28 17:22:50 -0800

    Changed live_devices_ to neighbors_ and nearby_device_comminicators to neighbor_communicators; uncommented and (hopefully) fixed establish_device_connections function; changed order of functions

--- include/heartbeat/Skynet_DeviceManager.hpp
+++ include/heartbeat/Skynet_DeviceManager.hpp
@@ -32,47 +32,63 @@ namespace skynet
 
 
    public:
-
-      DeviceManager(std::unique_ptr<Gateway> gateway):
+     /** \brief Construct a new DeviceManager
+      *
+      * \param gateway the devices gateway used for creating communications to
+      *  new devices
+      */
+     DeviceManager(std::unique_ptr<Gateway> gateway):
         gateway_(std::move(gateway))
       { }
 
-
-     /** \brief Construct a new DeviceManager
-      *
-      *	\param nearby_devices a vector of devices that DeviceManager
-      *	       will keep track of.
-      */
-    /* DeviceManager(std::vector<DeviceReference> nearby_devices)
-       : nearby_devices_(std::move(nearby_devices))
-     {
-       //Add empty vector of responses as initial value for each
-       // device to the response_history
-       for(auto device = begin(nearby_devices_); device!=end(nearby_devices_); ++device)
-    	 {
-    	   id_t device_id = device->get_id();
-    	   history_t init_history;
-    	   response_history_[device_id] = init_history;
-    	 }
-     }*/
-
      /** \brief DeviceManager destructor
       */
-     //QUESTION: Is the constructor too complicated for the default
-     // to work? Also is it okay to have virtual here?
-     virtual ~DeviceManager() = default;
+     ~DeviceManager() = default;
 
-     /** \brief Get devices that the DeviceManager currently believes are live
+
+     /** \brief Add a device to the list of neighboring devices and initialize its history
       *
-      * \return a vector of DeviceReferences
+      * \param new_device Device to add to the neighbors list
       */
-      const std::vector<DeviceReference>& get_live_devices() const
+     void add_device(DeviceReference& new_device)
      {
-       return live_devices_;
+        //Add new device to neighbors list
+        neighbors_.push_back(std::move(new_device));
+
+        //Add empty history for new device
+        id_t device_id = new_device.get_id();
+        history_t init_history;
+        response_history_[device_id] = init_history;
      }
 
+     /** \brief Remove a device from the list of neighboring devices and remove the
+      *  history associated with this device
+      *
+      * \param old_device device remove from neighbors list
+      */
+     //NOTE: We might want to think about what it means for a device to be
+     // removed. For now I'm assuming that if a device is removed it's removed
+     // from the neighbors list completely, so its history should be removed too.
+     // For DeviceManagers that continue to track dead devices, the history might
+     // not be removed once the device is pronounced dead, but the status of the
+     // device is stored in the DeviceReference, so in this case we wouldn't need
+     // to do anything since the status would be updated within DeviceReference
+     // and we would keep the dead device along with its  history in the
+     // using the neighbors list in the DeviceManager.
+     void remove_device(DeviceReference old_device)
+     {
+        /*AF: again commenting out, issue with types with the Device Reference
+       //Use erase on top of remove to shorten vector after removing device
+       neighbors_.erase(std::remove(neighbors_.begin(),neighbors_devices_.end(), old_device),
+			                 neighbors_.end());
 
-     /** \brief Collect new devices that have connected and add to list of devices.
+
+       //Remove device history
+       id_t device_id = old_device.get_id();
+       response_history_.erase(device_id);*/
+     }
+
+      /** \brief Collect new devices that have connected and add to list of neighbors
       */
       void collect_new_devices()
       {
@@ -89,58 +105,24 @@ namespace skynet
 
      void establish_device_connections()
      {
-      /*AF: This function was not compling, commented out for now */
-      // for (auto device = begin(nearby_devices_); device!=end(nearby_devices_); ++device)
-      // {
-      //   if (not nearby_device_communicators_.count(device->get_id()))
-      //   {
-      //   std::pair<const id_t, std::unique_ptr<DeviceCommunicator> p(device->get_id(), device.create_new_communicator());
-      //   nearby_device_communicators_.insert(std::move(p));
-      //   }
-      // }
+       for (auto device = begin(neighbors_); device!=end(neighbors_); ++device)
+       {
+         if (not neighbor_communicators_.count(device->get_id()))
+         {
+	   std::pair<const id_t, std::unique_ptr<DeviceCommunicator>>
+	     p(device->get_id(), device.create_new_communicator());
+	   neighbor_communicators_.insert(std::move(p));
+         }
+       }
     }
-
-     /** Add a device to the nearby devices list and initialize its history
+     
+     /** \brief Get devices that the DeviceManager currently believes are live
       *
-      * \param new_device Device to add to the nearby devices list
+      * \return a vector of DeviceReferences
       */
-     void add_device(DeviceReference& new_device)
+     const std::vector<DeviceReference>& get_neighbors() const
      {
-        //Add new device to device list
-        live_devices_.push_back(std::move(new_device));
-
-        //Add empty history for new device
-        id_t device_id = new_device.get_id();
-        history_t init_history;
-        response_history_[device_id] = init_history;
-     }
-
-     /** Remove a device from the nearby devices list and remove the history
-      *  associated with this device
-      *
-      * \param old_device device remove from nearby devices list
-      */
-     //NOTE: We might want to think about what it means for a device to be
-     // removed. For now I'm assuming that if a device is removed it's removed
-     // from the nearby_device list completely, so its history should be removed too.
-     // For DeviceManagers that continue to track dead devices, the history might
-     // not be removed once the device is pronounced dead, but the status of the
-     // device is stored in the DeviceReference, so in this case we wouldn't need
-     // to do anything since the status would be updated within DeviceReference
-     // and we would keep the dead device along with its  history in the
-     // using the nearby_devices_ list in the DeviceManager.
-     void remove_device(DeviceReference old_device)
-     {
-        /*AF: again commenting out, issue with types with the Device Reference
-       Use erase on top of remove to shorten vector after removing device
-       nearby_devices_.erase(std::remove(nearby_devices_.begin(),
-					 nearby_devices_.end(), old_device),
-			                 nearby_devices_.end());
-
-
-       //Remove device history
-       id_t device_id = old_deviceget_id();
-       response_history_.erase(device_id);*/
+       return neighbors_;
      }
 
      /** \brief Add response to response_history for the given device
@@ -177,15 +159,13 @@ namespace skynet
      }
 
    private:
-      std::vector<DeviceReference> live_devices_;
+      std::vector<DeviceReference> neighbors_;
       std::unordered_map<id_t, std::unique_ptr<DeviceCommunicator>>
-        nearby_device_communicators_;
+        neighbor_communicators_;
       std::unique_ptr<Gateway> gateway_;
       DeviceIdRegistry<id_t> id_registry_;
 
-     /** A map storing the response history for all live devices */
-     //QUESTION: Should response history be stored for live devices or known
-     // devices? This might depend on the type of  DeviceManager.
+     //A map storing the response history for all neighboring devices
      std::unordered_map<id_t, history_t> response_history_;
 
    };// class
