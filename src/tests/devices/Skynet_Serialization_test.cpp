bba5faed9512b6c7b331782db038fbc40bd082c9
Adam Harter <harter8@llnl.gov>
2019-08-30 13:44:36 -0700

    Add a handful more consts to serialization test and remove unneeded function.

--- src/tests/devices/Skynet_Serialization_test.cpp
+++ src/tests/devices/Skynet_Serialization_test.cpp
@@ -12,27 +12,19 @@ T test_serialization(const T& t)
   return deserialize<T>(serialize(t));
 }
 
-// Roundtrip a std::vector<T> through serialize/deserialize
-template<typename T>
-std::vector<T> test_serialization(const std::vector<T>& t)
-{
-  return deserialize<std::vector<T>>(serialize(t));
-}
-
-
 TEST_CASE("POD Serialization/Deserialization works", "[Skynet_Serializer]")
 {
-  int test_int = -2;
-  double test_double = 2.5;
-  unsigned test_unsigned = 3;
-  bool test_bool = true;
-  std::vector<int> intvec { 3, 2, 9 };
-  std::vector<double> doublevec { 3.2, 2.6, -9.3 };
+  const int test_int = -2;
+  const double test_double = 2.5;
+  const unsigned test_unsigned = 3;
+  const bool test_bool = true;
+  const std::vector<int> intvec{3, 2, 9};
+  const std::vector<double> doublevec{3.2, 2.6, -9.3};
 
-  int recovered_int = test_serialization(test_int);
-  double recovered_double = test_serialization(test_double);
-  unsigned recovered_unsigned = test_serialization(test_unsigned);
-  bool recovered_bool = test_serialization(test_bool);
+  const int recovered_int = test_serialization(test_int);
+  const double recovered_double = test_serialization(test_double);
+  const unsigned recovered_unsigned = test_serialization(test_unsigned);
+  const bool recovered_bool = test_serialization(test_bool);
 
   REQUIRE(test_int == recovered_int);
   REQUIRE(test_double == recovered_double);
@@ -42,13 +34,13 @@ TEST_CASE("POD Serialization/Deserialization works", "[Skynet_Serializer]")
 
 TEST_CASE("Vector of POD Serialization/Deserialization works", "[Skynet_Serializer]")
 {
-  std::vector<int> intvec { 3, -2, 9 };
-  std::vector<double> doublevec { 3.2, 2.6, -9.3 };
-  std::vector<unsigned> unsignedvec { 3, 1, 11 };
+  const std::vector<int> intvec{3, -2, 9};
+  const std::vector<double> doublevec{3.2, 2.6, -9.3};
+  const std::vector<unsigned> unsignedvec {3, 1, 11};
 
-  std::vector<int> recovered_intvec = test_serialization(intvec);
-  std::vector<double> recovered_doublevec = test_serialization(doublevec);
-  std::vector<unsigned> recovered_unsignedvec = test_serialization(unsignedvec);
+  const std::vector<int> recovered_intvec = test_serialization(intvec);
+  const std::vector<double> recovered_doublevec = test_serialization(doublevec);
+  const std::vector<unsigned> recovered_unsignedvec = test_serialization(unsignedvec);
 
   REQUIRE(intvec == recovered_intvec);
   REQUIRE(doublevec == recovered_doublevec);
