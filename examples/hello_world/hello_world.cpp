fffe8999ea00eebd66f494c891ba80c18c952be1
Adam Harter <harter8@llnl.gov>
2019-12-04 15:47:45 -0800

    First draft of comments for Hello World example application.

--- examples/hello_world/hello_world.cpp
+++ examples/hello_world/hello_world.cpp
@@ -7,17 +7,32 @@
 #include <random>
 #include <thread>
 
+// The names for each instance that is in this network
+// In this program, the network consists of just 5 machines
+// All names throughout a network must be unique
 constexpr std::array node_names{
   "node1", "node2", "node3", "node4", "node5"
 };
 
+// The port that each instance uses
+// All connections in this program are done locally
+// In addition to the base port, a second port that is 100 higher than the base
+// port is also used for the publication channels
 constexpr std::array<std::uint16_t, node_names.size()> node_ports{
   10000, 11000, 12000, 13000, 14000
 };
 
+// Tag that can be used to send a value for a reduce operation
+// This tag uses a signed 32-bit integer for the value to send
 using I32ValueTag = skynet::ReduceValueTag<std::int32_t>;
+
+// Tag that is used for creating reduction groups that can perform reduce operations
+// The type used for this tag must match the type used for the value tags
 using I32GroupTag = skynet::ReduceGroupTag<std::int32_t>;
 
+// The tags that will be participating in the reduce group
+// Each machine can only produce one tag for the reduce group, but all
+// of them must know about all tags that are being used for the group
 const std::vector<I32ValueTag> reduce_group_tags{
   I32ValueTag{"tag1"},
   I32ValueTag{"tag2"},
@@ -28,11 +43,15 @@ const std::vector<I32ValueTag> reduce_group_tags{
 
 int main(const int argc, const char* const argv[])
 {
+  // Error checking for the number of arguments
   if (argc != 2)
   {
     std::cerr << "Usage:\n" << argv[0] << " machine_index\n";
     return 1;
   }
+  // Parse the machine number that was passed in
+  // Do this in a lambda so that if there's an exception a dummy value can be
+  // returned which will always trigger an error
   const int machine_number = [&]() {
     try
     {
@@ -43,6 +62,7 @@ int main(const int argc, const char* const argv[])
       return -1;
     }
   }();
+  // Make sure that the machine number is valid, outputting an error message if not
   if (machine_number < 0 || machine_number >= static_cast<int>(node_ports.size()))
   {
     std::cerr
@@ -50,7 +70,19 @@ int main(const int argc, const char* const argv[])
       << "Must be an integer between 0 and " << node_ports.size() - 1 << '\n';
     return -1;
   }
-  skynet::Master master{node_ports[machine_number], node_names[machine_number]};
+  // Create a Skynet Master; the Master is responsible for handling communication
+  // and other such supporting tasks in the background
+  skynet::Master master{
+    // The port that the Master will listen for connections on
+    node_ports[machine_number],
+    // The name of the Master, each instance in the network must have a unique name
+    node_names[machine_number]
+  };
+  // Skynet currently has no way of automatically scanning for new machines while running
+  // It will accept any connection requests made to it, however, and the only requirement
+  // for it to function is that all nodes have paths to other nodes, so just have each
+  // instance connect to the higher numbered one, or for the highest numbered one, just
+  // advance to the job so the connection can be accepted
   if (machine_number != node_ports.size() - 1)
   {
     while (!master.connect_to_server("127.0.0.1", node_ports[machine_number + 1]))
@@ -58,12 +90,37 @@ int main(const int argc, const char* const argv[])
       std::this_thread::sleep_for(std::chrono::milliseconds{10});
     }
   }
+  // Submit work to the master, each job must have a unique name locally, but can be
+  // duplicated on other instances.  Jobs run on seperate threads than the master and
+  // are intended to be where computation and user-defined tasks are done.  Any
+  // callable object can be passed as a job, the only restrictions are that it must
+  // be copyable and the signature should be compatible with void(skynet::Job&)
   master.submit_job("job", [&](skynet::Job& job) {
-    auto reduce_group = job.create_reduce_group(
+    // Initiate the work to create a group that can perform reductions
+    // As creating this group is an expensive operation with a lot of communication,
+    // groups should be kept as long as they are needed and not be discarded as soon
+    // as a single reduce operation is finished
+    // This function also does not return the group itself, but a skynet::Future,
+    // meaning that the work has been initiated and will be finished on a seperate thread
+    auto reduce_group_fut = job.create_reduce_group(
+      // The tag for the reduce group, the same group tag and tags used for the
+      // reduce must be used for all instances taking part in the group, otherwise
+      // it will malfunction
       I32GroupTag{"random number reduce"},
+      // The tag that this machine is producing for the device, there can be multiple
+      // instances that produce the same tag, but each instance can only produce a single
+      // tag
       reduce_group_tags[machine_number],
+      // All of the tags that are to take part in the reduce
       reduce_group_tags
-    ).get();
+    );
+    // Retrieve the reduce group from the future; this will block until the group is
+    // finished being created; the returned object can the be used to perform reduce
+    // and allreduce operations
+    auto reduce_group = reduce_group_fut.get();
+    // For this example program, each instance will be generating random numbers
+    // and then they will be summed together; these next few lines of code are just
+    // setting up to do that
     std::uniform_int_distribution<std::int32_t> random_dist{50, 150};
     std::ranlux48 prng{std::random_device{}()};
     const auto min_value = static_cast<int>(random_dist.min() * node_names.size());
@@ -72,10 +129,30 @@ int main(const int argc, const char* const argv[])
     while (true)
     {
       const auto random_value = random_dist(prng);
+      // Initiate the allreduce operation, the second parameter is a callable to
+      // use, it can be anything that can be called that takes two parameters of
+      // the proper type (in this case, std::int32_t) and returns the same type
+      // Allreduce returns two futures, rather than one, each will be described
+      // below
       auto [send_ready, value_ready] = reduce_group.allreduce(random_value, std::plus<>{});
+      // The first future indicates that the value is ready to be sent to the parent node -
+      // the reduce operation is implemented as a binary tree, so this indicates that
+      // the two child values have been recieved and the operation is ready to be
+      // sent
+      // TODO: Even though this returns void, get() still needs to be called on it
+      // This is kind of unintuitive, but considering it's the low-level interface
+      // and can be wrapped into something nicer, that's maybe not a problem?
+      // If the actual computation is moved to the Master thread, rather than job
+      // threads, then this will no longer be an issue as a single future should be
+      // able to be used
       send_ready.get();
+      // The second future indicates that the final result of the reduction operation
+      // is ready to be retrieved; so just retrieve the value.  The first future must
+      // be finished first in order for this one to finish as
       const auto result = value_ready.get();
       const auto cur_time = std::time(nullptr);
+      // The result should never fall outside of the specified range, but do a sanity
+      // check just in case
       if (result >= min_value && result <= max_value)
       {
         std::cout
@@ -84,9 +161,12 @@ int main(const int argc, const char* const argv[])
       }
       else
       {
-        std::cout
+        // If this message is ever seen and the code has otherwise been unchanged,
+        // there's some kind of bug!
+        std::cerr
           << std::put_time(std::localtime(&cur_time), "[%F %T]")
           << " !!! Out of range value " << result << " !!!\n";
+        std::exit(1);
       }
       // Sleep for a random duration
       std::uniform_int_distribution<int> sleep_dist{10, 1000};
