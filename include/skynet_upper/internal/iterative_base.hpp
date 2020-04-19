74571ebf6411c8027ff6b32195272894da30dd23
Kendall Harter <harter8@llnl.gov>
2020-04-19 12:32:20 -0700

    More iterative functionality.

--- include/skynet_upper/internal/iterative_base.hpp
+++ include/skynet_upper/internal/iterative_base.hpp
@@ -11,7 +11,7 @@ namespace skynet::internal
   public:
     /** \brief Rebuilds connections for dead tags
      */
-    auto rebuild_dead_tags()
+    auto rebuild_dead_tags() noexcept
       -> Waiter<internal::MasterSubscribeIsDone, WaiterGetNoOp>
     {
       auto to_ret = job_->rebuild_tags(dead_tags_);
@@ -24,13 +24,79 @@ namespace skynet::internal
       return to_ret;
     }
 
+    /** \brief Rebuilds the specified dead tags, ignoring any tags that aren't dead
+     */
+    template<typename Range>
+    auto rebuild_dead_tags_range(const Range& r) noexcept
+      -> Waiter<internal::MasterSubscribeIsDone, WaiterGetNoOp>
+    {
+      std::vector<PublishTag<TagValueTypes...>> search_tags;
+      for (const auto& tag : r)
+      {
+        auto iter = std::find(dead_tags_.begin(), dead_tags_.end(), tag.id());
+        if (iter != dead_tags_.end())
+        {
+          search_tags.push_back(std::move(*iter));
+          dead_tags_.erase(*iter);
+        }
+      }
+      return job_->rebuild_tags(search_tags);
+    }
+
+    /** \brief Rebuilds the specified dead tags
+     */
+    template<typename... Tags>
+    auto rebuild_dead_tags(const Tags&... tags) noexcept
+      -> Waiter<internal::MasterSubscribeIsDone, WaiterGetNoOp>
+    //  requires (... && std::is_base_of_v<internal::PublishTagBase, Ts>)
+    {
+      const std::array<internal::PublishTagBase, sizeof...(Tags)> tag_array{
+        static_cast<internal::PublishTagBase>(tags)...
+      };
+      return rebuild_dead_tags_range(tag_array);
+    }
+
     /** \brief Drops tracking for dead tags
      */
-    void drop_dead_tags()
+    void drop_dead_tags() noexcept
     {
       // TODO: Actually unsubscribe when that's a thing that can happen
       dead_tags_.clear();
     }
+
+    /** \brief Drops tracking for specific tags, does nothing if the tags aren't dead
+     */
+    template<typename Range>
+    void drop_dead_tags(const Range& r) noexcept
+    {
+      for (const auto& tag : r)
+      {
+        const auto iter = std::find(dead_tags_.begin(), dead_tags_.end(), tag.id());
+        if (iter != dead_tags_.end())
+        {
+          dead_tags_.erase(iter);
+        }
+      }
+    }
+
+    /** \brief Drops the specified dead tags
+     */
+    template<typename... Tags>
+    void drop_dead_tags(const Tags&... tags) noexcept
+    //  requires (... && std::is_base_of_v<internal::PublishTagBase, Ts>)
+    {
+      const std::array<internal::PublishTagBase, sizeof...(Tags)> tag_array{
+        static_cast<internal::PublishTagBase>(tags)...
+      };
+    }
+
+    /** \brief Returns all active tags
+     */
+    const std::vector<PublishTag<TagValueTypes...>>& tags() const noexcept { return tags_; }
+
+    /** \brief Returns all dead tags
+     */
+    const std::vector<PublishTag<TagValueTypes...>>& dead_tags() const noexcept { return dead_tags_; }
 
   protected:
     IterativeBase(
