8e9ebeff719330db8024b8abbd2b9a8eb1b4691e
Barrett <barrett26@netleaf.llnl.gov>
2021-02-22 13:55:40 -0600

    9 agent csv example

--- examples/async_jacobi_header_general/async_jacobi_header_general.cpp
+++ examples/async_jacobi_header_general/async_jacobi_header_general.cpp
@@ -173,7 +173,7 @@ int main(int argc, char* argv[])
   std::string sol_file_name = argv[6];
   int number_of_updated_components = std::stoi(argv[7]);
   std::string root_pathway = argv[8];
-  int lc = std::stoi(argv[9]);
+  // int lc = std::stoi(argv[9]);
   //This creates the relevent vectors needed to interact with skynet.
   auto ports = set_port(starting_port_number, size_of_system);
   auto machine_names = obtain_machine_names(size_of_system);
@@ -184,11 +184,11 @@ int main(int argc, char* argv[])
   {
     row_indices.push_back((machine_number + i) % size_of_system);
   }
-  auto matrix_rows_hold = obtain_A_matrix(size_of_system, row_indices, matrix_file_name, root_pathway, lc);
+  // auto matrix_rows_hold = obtain_A_matrix(size_of_system, row_indices, matrix_file_name, root_pathway, lc);
 
-  auto b_values = obtain_rhs_vector(size_of_system, row_indices, rhs_file_name, root_pathway, lc);
+  // auto b_values = obtain_rhs_vector(size_of_system, row_indices, rhs_file_name, root_pathway, lc);
 
-  auto x_local_answer = obtain_local_ans_vector(size_of_system, row_indices, sol_file_name, root_pathway, lc);
+  // auto x_local_answer = obtain_local_ans_vector(size_of_system, row_indices, sol_file_name, root_pathway, lc);
   // This makes sure that the machine number and size_of_system is valid, and the dimension of the distributed b vector and matrix A match, outputting an error message if not.
   if (machine_number < 0 || machine_number >= static_cast<int>(ports.size()))
   {
@@ -220,9 +220,11 @@ int main(int argc, char* argv[])
   std::string my_file_name_local_forward_error="LocalIterateForwardErrorInformation" + std::to_string(machine_number) + ".csv";
 std::ofstream my_file_local_forward_error;
 my_file_local_forward_error.open(my_file_name_local_forward_error);
+std::cout << "This is my file name: " << my_file_name_local_forward_error << std::endl;
 if(my_file_local_forward_error.is_open()==false)
 {
   std::cout<<"This file is not open: "<<my_file_name_local_forward_error<<"."<<std::endl;
+  assert(my_file_local_forward_error.is_open()==true);
 }
 my_file_local_forward_error << "My rank ";
 my_file_local_forward_error << ",";
@@ -233,7 +235,7 @@ my_file_local_forward_error << ",";
 // my_file_local_forward_error << local_forward_error;
 my_file_local_forward_error << "\n";
 my_file_local_forward_error.close();
-  machine_task(machine_number, number_of_updated_components, matrix_rows_hold, b_values,  x_local_answer, row_indices, ports, machine_names, tags);
+  // machine_task(machine_number, number_of_updated_components, matrix_rows_hold, b_values,  x_local_answer, row_indices, ports, machine_names, tags);
   // std::cout << "This is after machine_task before return 0 for " << machine_number << std::endl;
 
   return 0;
